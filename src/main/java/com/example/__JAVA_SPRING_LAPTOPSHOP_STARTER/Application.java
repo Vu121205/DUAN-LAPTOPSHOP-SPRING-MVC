a82bcf620457eb6dd1522d3757ae2177992713cb
vuhoccode <vu03092019@gmail.com>
2025-09-20 20:13:27 +0700

    first commit

---
+++ src/main/java/com/example/__JAVA_SPRING_LAPTOPSHOP_STARTER/Application.java
@@ -0,0 +1,13 @@
+package com.example.__JAVA_SPRING_LAPTOPSHOP_STARTER;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/__JAVA_SPRING_LAPTOPSHOP_STARTER/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.__JAVA_SPRING_LAPTOPSHOP_STARTER;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
