805efb439fe4118335ee52b0340a4689c5d4716f
vuhoccode <vu03092019@gmail.com>
2025-09-21 09:55:02 +0700

    Add route use and admin

--- src/main/java/com/example/__JAVA_SPRING_LAPTOPSHOP_STARTER/HelloController.java
+++ src/main/java/com/example/__JAVA_SPRING_LAPTOPSHOP_STARTER/HelloController.java
@@ -3,12 +3,26 @@ package com.example.__JAVA_SPRING_LAPTOPSHOP_STARTER;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
 @RestController
 public class HelloController {
+
     @GetMapping("/")
     public String index()
     {
-        return "Hello world from Spring boot";
+        return "Hello world from Springboot";
+    }
+
+    @GetMapping("/user")
+    public String userPage()
+    {
+        return "Only user can access this page";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage()
+    {
+        return "Only admin can access this page";
     }
     
 }
--- src/main/java/com/example/__JAVA_SPRING_LAPTOPSHOP_STARTER/LaptopshopApplication.java
+++ src/main/java/com/example/__JAVA_SPRING_LAPTOPSHOP_STARTER/LaptopshopApplication.java
@@ -2,21 +2,13 @@ package com.example.__JAVA_SPRING_LAPTOPSHOP_STARTER;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class LaptopshopApplication {
 
 	public static void main(String[] args) {
 
-		
-
-		ApplicationContext vudoan = SpringApplication.run(LaptopshopApplication.class, args);
-
-		for(String s: vudoan.getBeanDefinitionNames())
-		{
-			System.out.println(s);
-		}
+		SpringApplication.run(LaptopshopApplication.class, args);
 	}
 
 }
