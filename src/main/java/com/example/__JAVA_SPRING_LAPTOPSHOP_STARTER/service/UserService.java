8ac642b4d5e43503310a6da789ae51f2c68c224d
vuhoccode <vu03092019@gmail.com>
2025-11-01 21:20:05 +0700

    tạo thư mục và bắt đầu code theo chương trình Dependence injection

---
+++ src/main/java/com/example/__JAVA_SPRING_LAPTOPSHOP_STARTER/service/UserService.java
@@ -0,0 +1,11 @@
+package com.example.__JAVA_SPRING_LAPTOPSHOP_STARTER.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    public String handleHello()
+    {
+        return "Hello from servive";
+    }
+}
